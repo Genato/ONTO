a9879747612c22e4bf61738e08c9eab5f3e02fac
Renato.Katalenic <druidhr@gmail.com>
2017-12-03 01:34:16 +0100

    Added Startup.cs with "OwinStartup" attribute and registered ConfigureAuth() middleware to OWIN pipeline.

---
+++ ONTO/App_Start/Startup.Auth.cs
@@ -0,0 +1,16 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ONTO
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+
+        }
+    }
+}
---
+++ ONTO/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ONTO.Startup))]
+
+namespace ONTO
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
